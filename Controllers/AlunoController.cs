49ba0c0a48021ab63838fec336d824f23ecdf726
Emerson de Paula Delfino <emersondepauladelfino@MacBook-Pro-de-Emerson.local>
2026-09-25 18:37:08 -0300

    Crud dotnet EntityFramework

--- Controllers/AlunoController.cs
+++ Controllers/AlunoController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using PocDotNetPostgresql.Data;
 using PocDotNetPostgresql.Models;
 namespace PocDotNetPostgresql.Controllers;
@@ -37,6 +38,72 @@ public class AlunoController : ControllerBase
         return CreatedAtAction(nameof(CreateAluno), new { aluno.Id}, aluno);
 
 	}
-	
+
+    ///api/Aluno
+    [HttpGet]
+	public async Task<IActionResult> GetAlunos() {
+
+		var alunos = await _context.Alunos.AsNoTracking()
+                             .ToListAsync();
+
+		return Ok(alunos);
+
+	}
+
+    ///api/Aluno/{id}
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetAlunoById(int id)
+    {
+
+        if (await _context.Alunos.FindAsync(id) == null) {
+            return NotFound();
+		}
+
+
+        return Ok(await _context.Alunos.FindAsync(id));
+
+    }
+
+    ///api/Aluno/{id}
+    [HttpPut("{id:int}")]
+	public async Task<IActionResult> update(int id, Alunos aluno) {
+
+		if (id != aluno?.Id) {
+			return BadRequest("Mismatched ID in URL and request body.");
+		}
+
+		_context.Entry(aluno).State = EntityState.Modified;
+
+		try{
+
+			await _context.SaveChangesAsync();
+
+		}catch (DbUpdateConcurrencyException) {
+            throw;
+        }
+
+        // HTTP 204 No Content
+        return NoContent();
+   
+	}
+
+    ///api/Aluno/{id}
+    [HttpDelete("{id:int}")]
+    public async Task<IActionResult> DeleteById(int id)
+    {
+        var aluno = await _context.Alunos.FindAsync(id);
+
+        if (aluno == null) {
+            return NotFound();
+        }
+
+        _context.Alunos.Remove(aluno);
+        await _context.SaveChangesAsync();
+
+        return NoContent();
+
+    }
+
+
 }
 
